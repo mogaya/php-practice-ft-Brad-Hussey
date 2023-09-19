d7118d261c5f9b0b6b98c9d0fa03cdb47488074e
mogaya <mogayainnocent@gmail.com>
2023-09-19 08:37:43 +0300

    If statements

--- 10_IfStatements/practice.php
+++ 10_IfStatements/practice.php
@@ -1,43 +1,56 @@
 <?php
-	
-	// Constants
 
-	
-	// Custom Variables
+// Constants
+define("TITLE", "If Statements");
 
+// Custom Variables
+$my_name = "Mogaya";
+$lesson_num = 10;
+
+$a = 20;
+$b = 50;
 
 ?>
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>PHP <!-- TITLE --></title>
-		<link href="../assets/styles.css" rel="stylesheet">
-	</head>
-	<body>
-		<div class="wrapper">
-			<a href="/" title="Back to directory" id="logo">
-				<img src="../assets/img/logo.png" alt="PHP">
-			</a>
-			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- TITLE --></small></h1>
-			<hr>
-			
-			<h2>Your Example</h2>
-			
-			<div class="sandbox">
-				
-			</div><!-- end sandbox -->
-			
-			<a href="index.php" class="button">Back to the lecture</a>
-			
-			<hr>
-			
-			<small>&copy;<!-- YEAR --> - <!-- NAME --></small>
-		</div><!-- end wrapper -->
-		
-		<div class="copyright-info">
-			<?php include('../assets/includes/copyright.php'); ?>
-		</div><!-- end copyright-info -->
-	</body>
+
+<head>
+	<title>PHP <?php echo TITLE; ?></title>
+	<link href="../assets/styles.css" rel="stylesheet">
+</head>
+
+<body>
+	<div class="wrapper">
+		<a href="/" title="Back to directory" id="logo">
+			<img src="../assets/img/logo.png" alt="PHP">
+		</a>
+
+		<h1>Tutorial <?php echo $lesson_num; ?>: <small><?php echo TITLE; ?></small></h1>
+		<hr>
+
+		<h2>Your Example</h2>
+
+		<div class="sandbox">
+			<?php
+			if ($a < $b) {
+				echo "Yep! $a is certainly less than $b ";
+			}
+			?>
+
+
+		</div><!-- end sandbox -->
+
+		<a href="index.php" class="button">Back to the lecture</a>
+
+		<hr>
+
+		<small>&copy;<?php echo date('Y'); ?> - <?php echo $my_name; ?></small>
+	</div><!-- end wrapper -->
+
+	<div class="copyright-info">
+		<?php include('../assets/includes/copyright.php'); ?>
+	</div><!-- end copyright-info -->
+</body>
+
 </html>

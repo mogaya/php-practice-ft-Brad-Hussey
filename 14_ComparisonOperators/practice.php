29b15539532bf65ef902b9e8c2829e8749a780c0
mogaya <mogayainnocent@gmail.com>
2023-09-28 22:00:26 +0300

    Comparison Operators

--- 14_ComparisonOperators/practice.php
+++ 14_ComparisonOperators/practice.php
@@ -1,9 +1,16 @@
 <?php
 	
 	// Constants
+	define("TITLE", "Comparison Operators");
 
 	
 	// Custom Variables
+	$my_name = "Mogaya";
+	$lesson_num = 14;
+
+	$yearsOnEarth = 25.32;
+	$favouriteStringNum = "1";
+	$birthCountry = "Canada";
 
 
 ?>
@@ -11,7 +18,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP <!-- TITLE --></title>
+		<title>PHP <?php echo TITLE; ?></title>
 		<link href="../assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
@@ -20,7 +27,7 @@
 				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
 			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- TITLE --></small></h1>
+			<h1>Tutorial <?php echo $lesson_num; ?>: <small><?php echo TITLE; ?></small></h1>
 			<hr>
 			
 			<h2>Your Example</h2>
@@ -29,42 +36,74 @@
 				
 				<h3>Equal <code>==</code></h3>
 				<?php
-					// your code here
+					if ($yearsOnEarth = 25.32){
+						echo "Your Age is $yearsOnEarth";
+					}
 				?>
 				
 				<h3>Identical <code>===</code></h3>
 				<?php
-					// your code here
+					if ($favouriteStringNum === 1){
+
+						echo "Your fav number is an integer";
+
+					}elseif($favouriteStringNum === '1'){
+
+						echo "Your fav number is a string";
+
+					}else {
+
+						echo "Your fav number is different";
+
+					}
 				?>
 				
 				<h3>Not Equal <code>!=</code></h3>
 				<?php
-					// your code here
+					if ($birthCountry != "Mexico") {
+						echo "Excuse me sir you must not be from around here";
+					}
 				?>
 				
 				<h3>Not Identical <code>!==</code></h3>
 				<?php
-					// your code here
+					if ($yearsOnEarth !== "25.32") {
+
+						echo "You are not exactly the string \"$yearsOnEarth\"";
+
+					}else {
+
+						echo "You are exactly the string \"$yearsOnEarth\"";
+
+					}
 				?>
 				
 				<h3>Less Than <code>&lt;</code></h3>
 				<?php
-					// your code here
+					if ($lesson_num < 15){
+						echo "You haven't quite made it to lesson 15";
+					}
 				?>
 				
 				<h3>Greater Than <code>&gt;</code></h3>
 				<?php
-					// your code here
+					if ($lesson_num > 10){
+						echo "You've made it past lesson 10";
+					}
 				?>
 				
 				<h3>Less Than or Equal To <code>&lt;=</code></h3>
 				<?php
-					// your code here
+					if ($lesson_num <= 14) {
+						echo "$lesson_num is less or equal to 14";
+					}
 				?>
 				
 				<h3>Greater Than or Equal To <code>&gt;=</code></h3>
 				<?php
-					// your code here
+					if ($lesson_num >= 14) {
+						echo "$lesson_num is greater or equal to 4";
+					}
 				?>
 				
 			</div><!-- end sandbox -->
@@ -73,7 +112,7 @@
 			
 			<hr>
 			
-			<small>&copy;<!-- YEAR --> - <!-- NAME --></small>
+			<small>&copy;<?php echo date('Y'); ?> - <?php echo $my_name; ?></small>
 		</div><!-- end wrapper -->
 		
 		<div class="copyright-info">
--- 15_LogicalOperators/practice.php
+++ 15_LogicalOperators/practice.php
@@ -1,9 +1,18 @@
 <?php
 	
 	// Constants
+	define("TITLE", "Logical Operators");
 
 	
 	// Custom Variables
+	$my_name = "Mogaya";
+	$lesson_num = 15;
+
+	$username = "johnyboy";
+	$password = "qwerty";
+
+	$cartTotal = 19.99;
+	$couponCode = "DiscountPlease";
 
 
 ?>
@@ -11,7 +20,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP <!-- TITLE --></title>
+		<title>PHP <?php echo TITLE;?></title>
 		<link href="../assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
@@ -20,7 +29,7 @@
 				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
 			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- TITLE --></small></h1>
+			<h1>Tutorial <?php echo $lesson_num;?>: <small><?php echo TITLE;?></small></h1>
 			<hr>
 			
 			<h2>Your Example</h2>
@@ -58,7 +67,7 @@
 			
 			<hr>
 			
-			<small>&copy;<!-- YEAR --> - <!-- NAME --></small>
+			<small>&copy;<<?php echo date('Y');?> - <?php echo $my_name;?></small>
 		</div><!-- end wrapper -->
 		
 		<div class="copyright-info">

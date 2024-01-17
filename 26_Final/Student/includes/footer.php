5004349b6988f7ccd163f9347ca3fc8921d2e111
mogaya <mogayainnocent@gmail.com>
2024-01-17 15:46:20 +0300

    Global header and footer

---
+++ 26_Final/Student/includes/footer.php
@@ -0,0 +1,43 @@
+
+<div id="footer" class="cf">
+                <div class="column three">
+                    <strong>Phone</strong>
+                    +254712345678
+
+                </div>
+                <!-- column -->
+                <div class="column three">
+                    <strong>Phone</strong>
+                    123 Syoki Nairobi <br>
+                    Kenya, KE
+
+                </div>
+                <!-- column -->
+                <div class="column three last">
+                    <strong>Hours</strong>
+                    <em>Tuesday - Thursday</em> <br>
+                    1:00pm - 9:00pm <br><br>
+
+                    <em>Friday - Saturday</em> <br>
+                    4:00pm - 11:00pm <br><br>
+
+                    <em>Sunday - Monday</em> <br>
+                    Closed <br><br>
+
+                </div>
+                <!-- column -->
+
+            </div>
+            <!-- footer -->
+            <small>&copy; 2000 Mogaya</small>
+        </div>
+        <!-- content -->
+    </div>
+    <!-- wrapper -->
+    <div class="copyright-info">
+        <?php ?>
+    </div>
+    <!-- copyright-info -->
+</body>
+
+</html>
---
+++ 26_Final/Student/includes/header.php
@@ -0,0 +1,30 @@
+<?php 
+    include('includes/arrays.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body id="final-example">
+    <div class="wrapper">
+        <div id="banner">
+            <a href="/" title="Return to Home">
+                <img src="img/banner.png" alt="Franklin's Fine Dining">
+            </a>
+
+        </div>
+        <!-- banner -->
+        <div id="nav">
+
+                <?php include('includes/nav.php'); ?>
+
+        </div>
+        <!-- nav -->
+        <div class="content">

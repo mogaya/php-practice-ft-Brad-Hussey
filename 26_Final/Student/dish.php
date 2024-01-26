5453664907b40f256a5e79f6980a0f2bf6c5cf30
mogaya <mogayainnocent@gmail.com>
2024-01-26 19:44:13 +0300

    dish progress

--- 26_Final/Student/dish.php
+++ 26_Final/Student/dish.php
@@ -7,3 +7,29 @@ function strip_bad_chars($input)
     $output = preg_replace("/[^a-zA-Z0-9_-]/", "", $input);
     return $output;
 }
+
+if (isset($_GET['item'])) {
+    $menuItem = strip_bad_chars($_GET['item']);
+    $dish = $menuItems[$menuItem];
+}
+
+function suggestedTip($price, $tip)
+{
+
+    $totalTip = $price * $tip;
+    echo money_format('%.2n', $totalTip);
+}
+
+?>
+
+<hr>
+<div id="dish">
+    <h1><?php echo $dish["title"]; ?> <span class="price"><sup>ksh</sup><?php echo $dish["price"]; ?></span></h1>
+    <p><?php echo $dish["blurb"] ?></p>
+
+    <br>
+
+    <p><strong>Suggested Beverage <?php echo $dish["drink"] ?></strong></p>
+    <p><em>Suggested Tip <sup>ksh</sup></em></p>
+</div>
+<!-- dish -->
